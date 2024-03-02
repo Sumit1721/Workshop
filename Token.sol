6f0ccbdc9413abdbf157089a962dfb674c504d1d
Sumit1721 <160008299+Sumit1721@users.noreply.github.com>
2024-03-02 13:16:03 +0530

    Create Token.sol

---
+++ Token.sol
@@ -0,0 +1,21 @@
+// SPDX-licence-Identifier: MIT;
+pragma solidity 0.8.24;
+
+contract Token {
+    string public TokenName = "Sumit";
+    string public tokenABBv = "Ssg";
+    uint public totalSupply= 0;
+
+    mapping(address => uint)public balances;
+    function mint(address _address, uint _value)public{
+        balances[_address] += _value;
+        totalSupply += _value;
+    }
+
+    function burn(address _address, uint _value)public{
+        if (balances[_address]>= _value){
+            balances[_address]-= _value;
+            totalSupply -= _value;
+        }
+    }
+}
